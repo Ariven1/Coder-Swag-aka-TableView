fb7e60636c20727a332f19f6c94392139594de83
Ariven <ariven@Arivens-Mac.local>
2017-08-27 13:27:02 +0530

    lesson03 - CollectionView

---
+++ coder-swag/ProductCVCell.swift
@@ -0,0 +1,16 @@
+//
+//  ProductCVCell.swift
+//  coder-swag
+//
+//  Created by Ariven on 27/08/17.
+//  Copyright © 2017 Ariven. All rights reserved.
+//
+
+import UIKit
+
+class ProductCVCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
